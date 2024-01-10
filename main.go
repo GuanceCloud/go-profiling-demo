b722e2febfed146dcfc047571e942c1db83e5522
zhangyi <zhangyi905@guance.com>
2024-01-10 20:01:13 +0800

    Add more loading pressure

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"compress/gzip"
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/pprof"
 	"os"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -87,6 +90,61 @@ func isENVTrue(key string) bool {
 	return true
 }
 
+func fibonacci(ctx context.Context, n int) int {
+	if n <= 2 {
+		return 1
+	}
+	if n%31 == 0 {
+		return fibonacciWithTrace(ctx, n-1) + fibonacci(ctx, n-2)
+	} else if n%37 == 0 {
+		return fibonacciWithTrace(ctx, n-1) + fibonacciWithTrace(ctx, n-2)
+	}
+	return fibonacci(ctx, n-1) + fibonacci(ctx, n-2)
+}
+
+func fibonacciWithTrace(ctx context.Context, n int) int {
+	span, newCtx := tracer.StartSpanFromContext(ctx, "fibonacci")
+	defer span.Finish()
+	return fibonacci(newCtx, n-1) + fibonacci(newCtx, n-2)
+}
+
+func httpReqWithTrace(ctx context.Context) {
+	span, newCtx := tracer.StartSpanFromContext(ctx, "sendHttpRequest")
+	defer span.Finish()
+
+	bodyText := `
+黄河远上白云间，一片孤城万仞山。
+羌笛何须怨杨柳，春风不度玉门关。
+少小离家老大回，乡音无改鬓毛衰。
+儿童相见不相识，笑问客从何处来。
+`
+	for i := 0; i < 10; i++ {
+		func() {
+			req, err := http.NewRequestWithContext(newCtx, http.MethodGet, "https://tv189.com/", strings.NewReader(strings.Repeat(bodyText, 1000)))
+
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			log.Println(string(body))
+		}()
+	}
+}
+
 func main() {
 
 	if isENVTrue("DD_TRACE_ENABLED") {
@@ -105,6 +163,7 @@ func main() {
 				profiler.BlockProfile,
 				profiler.MutexProfile,
 				profiler.GoroutineProfile,
+				profiler.MetricsProfile,
 			),
 		)
 
@@ -119,15 +178,29 @@ func main() {
 	router.Use(gintrace.Middleware("go-profiling-demo"))
 
 	router.GET("/movies", func(ctx *gin.Context) {
+		span, traceCtx := tracer.StartSpanFromContext(ctx.Request.Context(), "get_movies")
+		defer span.Finish()
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func(ctx context.Context) {
+			defer wg.Done()
+			param := 42
+			log.Printf("fibonacci(%d) = %d\n", param, fibonacci(ctx, param))
+		}(traceCtx)
+
+		go func(ctx context.Context) {
+			defer wg.Done()
+			httpReqWithTrace(ctx)
+		}(traceCtx)
+
 		q := ctx.Request.FormValue("q")
 
 		moviesCopy := make(Movies, len(movies))
 		copy(moviesCopy, movies)
 
-		rc := ctx.Request.Context()
-		span, _ := tracer.StartSpanFromContext(rc, "sort")
 		sort.Sort(moviesCopy)
-		span.Finish()
 
 		if q != "" {
 			q = strings.ToUpper(q)
@@ -146,25 +219,30 @@ func main() {
 			log.Printf("encode into json fail: %s", err)
 			ctx.Writer.WriteHeader(http.StatusInternalServerError)
 		}
+		wg.Wait()
 	})
 
-	pprofGroup := router.Group("/debug/pprof")
-	pprofGroup.GET("/", func(ctx *gin.Context) {
+	pprofIndex := func(ctx *gin.Context) {
 		pprof.Index(ctx.Writer, ctx.Request)
+	}
+
+	router.GET("/debug/pprof", func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusMovedPermanently, "/debug/pprof/")
 	})
-	pprofGroup.GET("/:name", func(ctx *gin.Context) {
-		pprof.Index(ctx.Writer, ctx.Request)
-	})
-	pprofGroup.GET("/cmdline", func(ctx *gin.Context) {
+
+	pg := router.Group("/debug/pprof")
+	pg.GET("/", pprofIndex)
+	pg.GET("/:name", pprofIndex)
+	pg.GET("/cmdline", func(ctx *gin.Context) {
 		pprof.Cmdline(ctx.Writer, ctx.Request)
 	})
-	pprofGroup.GET("/profile", func(ctx *gin.Context) {
+	pg.GET("/profile", func(ctx *gin.Context) {
 		pprof.Profile(ctx.Writer, ctx.Request)
 	})
-	pprofGroup.GET("/symbol", func(ctx *gin.Context) {
+	pg.GET("/symbol", func(ctx *gin.Context) {
 		pprof.Symbol(ctx.Writer, ctx.Request)
 	})
-	pprofGroup.GET("/trace", func(ctx *gin.Context) {
+	pg.GET("/trace", func(ctx *gin.Context) {
 		pprof.Trace(ctx.Writer, ctx.Request)
 	})
 
